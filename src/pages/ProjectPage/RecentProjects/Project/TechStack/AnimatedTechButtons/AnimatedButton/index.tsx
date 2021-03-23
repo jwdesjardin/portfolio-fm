7c967442e665f99047380b647133aa1198e07569
joey desjardins <jwdesjardin@gmail.com>
2021-03-23 11:34:54 -0700

    add components: CustomModal, add animations: animatedButtons

--- src/pages/ProjectPage/RecentProjects/Project/TechStack/AnimatedTechButtons/AnimatedButton/index.tsx
+++ src/pages/ProjectPage/RecentProjects/Project/TechStack/AnimatedTechButtons/AnimatedButton/index.tsx
@@ -1,17 +1,39 @@
 import * as React from 'react'
 import { Center, Circle, Text } from '@chakra-ui/react'
 import { KeyboardArrowDown } from '@material-ui/icons'
+import { motion } from 'framer-motion'
 
-export const AnimatedButton = () => {
+interface AnimatedButtonProps {
+  index: number
+}
+
+export const AnimatedButton = ({ index = 0 }) => {
   const ChevronDownIcon = () => (
     <KeyboardArrowDown style={{ fontSize: '1.2rem' }}></KeyboardArrowDown>
   )
+
+  const MotionCircle = motion(Circle)
+
+  const colors = ['#067BDB', '#8a2cc9', '#2ec486', '#ff9f1c']
+
   return (
-    <Circle size='125px' bg='white' color='black'>
+    <MotionCircle
+      size='125px'
+      bg='white'
+      color='black'
+      whileHover={{
+        backgroundColor: colors[index % 4],
+        color: '#FFF',
+        y: [10, 0, 5, 0],
+        rotate: [0, 5, -5, 5, 0],
+      }}
+      whileClick={{ scale: 0.1, opacity: 0 }}
+      onClick={() => console.log('clicky')}
+    >
       <Center flexDirection='column'>
         <Text fontWeight='semibold'>Typescript</Text>
         <ChevronDownIcon></ChevronDownIcon>
       </Center>
-    </Circle>
+    </MotionCircle>
   )
 }
--- src/pages/ProjectPage/RecentProjects/Project/TechStack/AnimatedTechButtons/index.tsx
+++ src/pages/ProjectPage/RecentProjects/Project/TechStack/AnimatedTechButtons/index.tsx
@@ -2,20 +2,23 @@ import * as React from 'react'
 import { Center, Grid } from '@chakra-ui/react'
 
 import { AnimatedButton } from './AnimatedButton'
+import { CustomModal } from './CustomModal'
 
 export const AnimatedTechButtons = () => {
   return (
     <Center w='100%' h='330px'>
       <Grid gridGap={5} gridTemplateColumns='repeat(2, 1fr)'>
         {/* box #1 */}
-        <AnimatedButton></AnimatedButton>
+        <AnimatedButton index={0}></AnimatedButton>
+
         {/* box #2 */}
-        <AnimatedButton></AnimatedButton>
+        <AnimatedButton index={1}></AnimatedButton>
         {/* box #3 */}
-        <AnimatedButton></AnimatedButton>
+        <AnimatedButton index={2}></AnimatedButton>
         {/* box #4 */}
-        <AnimatedButton></AnimatedButton>
+        <AnimatedButton index={3}></AnimatedButton>
       </Grid>
+      <CustomModal></CustomModal>
     </Center>
   )
 }
